62fdef884bea481334834d7ca366c753d0d756e8
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-23 01:04:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @team = params[:name]
+    @name = params[:name]
     @coach = params[:coach]
     @pguard = params[:pg]
     @sguard = params[:sg]
